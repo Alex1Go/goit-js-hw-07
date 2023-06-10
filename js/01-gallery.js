271e53810f54cc170469ce11e844aee9859bbb48
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-06-11 00:07:07 +0300

    zxc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></li>`);
+galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+galleryEl.addEventListener('click', onClick);
+function onClick(evt) {
+    evt.preventDefault()
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+`);
+    instance.show();
+}
